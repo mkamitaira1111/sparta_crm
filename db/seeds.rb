37af77a1b99510f59c056da2951adca53d4c2938
madoka <m.kamitaira@gmail.com>
2018-03-18 11:22:47 +0900

    crud完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+100.times do |index|
+  Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+end# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
