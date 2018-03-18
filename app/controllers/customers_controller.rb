9c115713a7c17c4f8bd6dc72822b9bfbbe970f1d
madoka <m.kamitaira@gmail.com>
2018-03-18 15:17:10 +0900

    ransackまで実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,10 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy] #追加
 
   def index
-    @customers = Customer.page(params[:page])
+
+    @q = Customer.includes(:post, :company).ransack(params[:q])
+    @customers = @q.result.page(params[:page])
+
   end
 
   def new
@@ -45,7 +48,8 @@ class CustomersController < ApplicationController
       :family_name,
       :given_name,
       :email,
-      :company_id
+      :company_id,
+      :post_id
       )
   end
 
